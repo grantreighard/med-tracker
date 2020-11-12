8ded053ac40770ec01a9ed9258f06cba10b826ac
Grant Reighard <grant.reighard@gmail.com>
2020-11-11 21:13:44 -0500

    Adds Time Apart column to table, with working time calculation.

--- src/components/trackerList.js
+++ src/components/trackerList.js
@@ -48,6 +48,74 @@ export default function TrackerList(props) {
         return self.indexOf(value) === index;
     }
 
+    const convertTimeTo24Hours = (timeWithAmOrPm) => {
+        const amOrPm = timeWithAmOrPm.split(" ")[1];
+        const time = timeWithAmOrPm.split(" ")[0];
+        if (amOrPm === "am") {
+            return time;
+        } else {
+            const timeArr = time.split(":");
+            const hours = timeArr[0];
+            const minutes = timeArr[1];
+            const seconds = timeArr[2];
+
+            return `${Number(hours)+12}:${minutes}:${seconds}`;
+        }
+    }
+
+    const formatTimeNicely = (numSeconds) => {
+        let days = 0;
+        let hours = 0;
+        let minutes = 0;
+        let seconds = 0;
+
+        let timeLeft = numSeconds;
+        
+        if (timeLeft / (3600 * 24) > 0) {
+            days = Math.floor(timeLeft / (3600 * 24))
+            timeLeft -= days * (3600 * 24);
+        }
+        
+        if (timeLeft / 3600 > 0) {
+            hours = Math.floor(timeLeft / 3600)
+            timeLeft -= hours * 3600;
+        }
+        
+        if (timeLeft >= 60) {
+            minutes = Math.floor(timeLeft / 60);
+            timeLeft -= minutes * 60;
+        }
+        
+        seconds = timeLeft;
+        
+        return `${days > 0 ? days + "d " : ""} ${hours > 0 ? hours + "h " : ""} ${minutes > 0 ? minutes + "m " : ""} ${seconds > 0 ? seconds + "s" : ""}`
+    }
+
+    const getTimeDifference = (medication, index) => {
+        const currentDate = list[index].date.split("\"")[1].split("T")[0];
+        const currentTime = convertTimeTo24Hours(list[index].time);
+        const currentDateObj = new Date(`${currentDate}T${currentTime}`);
+
+        let foundIndex = -1;
+        for (let i = list.length-1; i >= 0; i--) {
+            if (list[i].medication.trim() === medication.trim() && i > index) {
+                foundIndex = i;
+            }
+        }
+
+        if (foundIndex >= 0) {
+            const foundDate = list[foundIndex].date.split("\"")[1].split("T")[0];
+            const foundTime = convertTimeTo24Hours(list[foundIndex].time);
+            const foundDateObj = new Date(`${foundDate}T${foundTime}`);
+
+            const differenceInMilliseconds = currentDateObj - foundDateObj;
+            const seconds = differenceInMilliseconds / 1000;
+            return formatTimeNicely(seconds);
+        } else {
+            return 'no prior record'
+        }
+    }
+
     return (
         <div className="tracker-list">
             <form onSubmit={formSubmit}>
@@ -77,10 +145,11 @@ export default function TrackerList(props) {
                         <th>Date</th>
                         <th>Time</th>
                         <th>Medication</th>
+                        <th>Time Apart</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {list && list.map((item) => {
+                    {list && list.map((item, i) => {
                         let date;
                         if (item.date) {
                             date = item.date.split("\"")[1].split("T")[0];
@@ -90,12 +159,13 @@ export default function TrackerList(props) {
                             <td>{date ? `${date.split("-")[1]}/${date.split("-")[2]}/${date.split("-")[0]}`: null}</td>
                             <td>{item.time ? item.time : null}</td>
                             <td>{item.medication ? item.medication : null}</td>
+                            <td>{getTimeDifference(item.medication, i)}</td>
                         </tr>
                     })}
                 </tbody>
             </table>
             <button onClick={clearTable} className="warn-button">Clear table</button>
-            <p>Copyright &copy; {new Date().getFullYear()} Reighard Enterprises. Please share.</p>
+            <p>Copyright &copy; {new Date().getFullYear()} Reighard Enterprises.</p>
             <Twitter link="https://www.popmypills.com" name="Share on Twitter" message="Check out this free medication tracker that you can save as a bookmark to your phone." label={serviceName => serviceName} />
         </div>
     )
