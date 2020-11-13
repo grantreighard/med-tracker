8daa3c3d9faa3c5c2109c5a78895410ac2912c53
Grant Reighard <grant.reighard@gmail.com>
2020-11-12 22:25:01 -0500

    Adds scroll position to keep modal centered vertically.

--- src/components/confirmationModal.js
+++ src/components/confirmationModal.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 export default function ConfirmationModal(props) {
+    const [scrollY, setScrollY] = useState(0);
+    useScrollPosition(({ prevPos, currPos }) => {
+        setScrollY(currPos.y);
+    })
+
     if (props.isShown) {
-        return <div className="confirmation-modal">
+        return <div className="confirmation-modal" style={{top: -scrollY}}>
             <p>Do you really want to delete this record?</p>
             <button onClick={props.yesFn} className="warn-button">Yes</button>
             <button onClick={props.noFn}>Cancel</button>
